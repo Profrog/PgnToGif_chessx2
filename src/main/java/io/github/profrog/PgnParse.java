6ef9641090a862bd3879238681ac71cb0dc9f556
profrog <ache159@naver.com>
2024-04-22 00:54:07 +0900

    piece_control

--- src/main/java/io/github/profrog/PgnParse.java
+++ src/main/java/io/github/profrog/PgnParse.java
@@ -26,6 +26,8 @@ public class PgnParse {
     public static List<String> white_move;
     public static  List<String> black_move;
 
+    public static  StringBuilder[] pawn_first_move = new StringBuilder[2];
+
     public static final int board_size = 8; //in normally, chess board size is 8x8
     public static int left_p = 0; //left column basis index
     public static int right_p = board_size-1; //right column basis index
@@ -34,85 +36,30 @@ public class PgnParse {
 
     public static List<int[][]> move_condition;
 
-
-    public static String pgn_example = "\n" +
-            "[Event \"Live Chess\"]\n" +
-            "[Site \"Chess.com\"]\n" +
-            "[Date \"2024.03.23\"]\n" +
-            "[Round \"?\"]\n" +
-            "[White \"MagicRoss\"]\n" +
-            "[Black \"PFchessX2\"]\n" +
-            "[Result \"0-1\"]\n" +
-            "[ECO \"D02\"]\n" +
-            "[WhiteElo \"1139\"]\n" +
-            "[BlackElo \"1150\"]\n" +
-            "[TimeControl \"120+1\"]\n" +
-            "[EndTime \"19:04:44 PDT\"]\n" +
-            "[Termination \"PFchessX2 won by checkmate\"]\n" +
-            "\n" +
-            "1. d4 d5 2. Nf3 Nc6 3. e3 Bf5 4. Bb5 a6 5. O-O e6 6. Bxc6+ bxc6 7. c3 Bd6 8. Re1\n" +
-            "Nf6 9. h3 h6 10. Nh4 Bh7 11. g4 Ne4 12. Nd2 Qxh4 13. Nxe4 Bxe4 14. f3 Qg3+ 15.\n" +
-            "Kf1 Bxf3 16. Qd2 Qxh3+ 17. Kg1 Qh1+ 18. Kf2 Qg2# 0-1\n";
-
-    public static String pgn_example2 = "\n" +
-            "[Event \"Casual 긴 대국 game\"]\n" +
-            "[Site \"https://lichess.org/3FLP6aWA\"]\n" +
-            "[Date \"2024.04.02\"]\n" +
-            "[White \"lichess AI level 1\"]\n" +
-            "[Black \"profrog\"]\n" +
-            "[Result \"0-1\"]\n" +
-            "[UTCDate \"2024.04.02\"]\n" +
-            "[UTCTime \"14:59:54\"]\n" +
-            "[WhiteElo \"?\"]\n" +
-            "[BlackElo \"1500\"]\n" +
-            "[Variant \"Standard\"]\n" +
-            "[TimeControl \"-\"]\n" +
-            "[ECO \"C44\"]\n" +
-            "[Opening \"Ponziani Opening\"]\n" +
-            "[Termination \"Normal\"]\n" +
-            "[Annotator \"lichess.org\"]\n" +
-            "\n" +
-            "1. e4 e5 2. Nf3 Nc6 3. c3 { C44 Ponziani Opening } Qf6 " +
-            "4. d4 exd4 5. cxd4 Bb4+ 6. Ke2 Nh6 7. Nc3 Ng4 8. a3 Nxd4+ 9. Kd2 Bxc3+ 10. Kd3 Nxf2+ " +
-            "11. Kc4 Nxd1 12. Rb1 b5+ 13. Kd5 Bb7+ 14. Kc5 d6# { Black wins by checkmate. } 0-1\n";
-
-
+    public static String chess_piece_line = "_♟♞♝♜♛♚♙♘♗♖♕♔*";
 
 
     //void parser_init(Image[] chess_img0, Color boardline_lout0, String pgn_data0)
-    public static void parserInit(Image[] chess_img0, Color boardline_lout0, String pgn_data0,int wanttosee_table)
+
+    public PgnParse()
     {
-        cur_chess_table = new int[8][8];
-        chess_img = new Image[chess_img0.length];
-        pgn_state = new HashMap<String,String>();
-
-        for(int idx = 0; idx < chess_img0.length; ++idx)
-        {
-            chess_img[idx] = chess_img0[idx];
-        }
-
-        boardline_lout = boardline_lout0;
-        pgn_data = pgn_data0;
-
-        //chess_image[0] = board_image.png..jpg..bmp..
-        //chess_image[1] = white_pawn_image.png..jpg..bmp..
-        //chess_image[2] = white_knight_image.png..jpg..bmp..
-        //chess_image[3] = white_bishop_image.png..jpg..bmp..
-        //chess_image[4] = white_rook_image.png..jpg..bmp..
-        //chess_image[5] = white_queen_image.png..jpg..bmp..
-        //chess_image[6] = white_king_image.png..jpg..bmp..
-
-        //chess_image[7] = black_pawn_image.png..jpg..bmp..
-        //chess_image[8] = black_knight_image.png..jpg..bmp..
-        //chess_image[9] = black_bishop_image.png..jpg..bmp..
-        //chess_image[10] = black_rook_image.png..jpg..bmp..
-        //chess_image[11] = black_queen_image.png..jpg..bmp..
-        //chess_image[12] = black_king_image.png..jpg..bmp..
+        logger.info("parser init");
     }
 
     public static void onlyParser(String pgn_data0, int black_bottom_opt, int want_seeing_table_opt)
     {
         try {
+
+
+            check_table_mem = new ArrayList<>();
+            pgn_state = new HashMap<String, String>();
+            white_move = new ArrayList<>();
+            black_move = new ArrayList<>();
+            piece_data = new HashMap<>();
+            pawn_first_move[0] = new StringBuilder("00000000");
+            pawn_first_move[1] = new StringBuilder("00000000");
+
+
             cur_chess_table = new int[][]{ //definition first state of board, white piece 1~6, black piece 1~6
                     {10,8,9,11,12,9,8,10},
                     {7,7,7,7,7,7,7,7},
@@ -120,17 +67,16 @@ public class PgnParse {
                     {0,0,0,0,0,0,0,0},
                     {0,0,0,0,0,0,0,0},
                     {0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0},
                     {1,1,1,1,1,1,1,1},
                     {4,2,3,5,6,3,2,4},
             };
+
             check_table_mem.add(cur_chess_table);
 
-            pgn_state = new HashMap<String, String>();
-            white_move = new ArrayList<>();
-            black_move = new ArrayList<>();
-            piece_data = new HashMap<>();
+            if(want_seeing_table_opt > 0)
+            {
+                showTableValue(check_table_mem.size()-1);
+            }
 
 
             piece_data.put("P",1); //piece meta data is added
@@ -140,23 +86,23 @@ public class PgnParse {
             piece_data.put("Q",5);
             piece_data.put("K",6);
 
-            piece_data.put("8",8);
-            piece_data.put("7",7);
-            piece_data.put("6",6);
-            piece_data.put("5",5);
-            piece_data.put("4",4);
-            piece_data.put("3",3);
-            piece_data.put("2",2);
-            piece_data.put("1",1);
+            piece_data.put("8",7);
+            piece_data.put("7",6);
+            piece_data.put("6",5);
+            piece_data.put("5",4);
+            piece_data.put("4",3);
+            piece_data.put("3",2);
+            piece_data.put("2",1);
+            piece_data.put("1",0);
 
-            piece_data.put("h",8);
-            piece_data.put("g",7);
-            piece_data.put("f",6);
-            piece_data.put("e",5);
-            piece_data.put("d",4);
-            piece_data.put("c",3);
-            piece_data.put("b",2);
-            piece_data.put("a",1);
+            piece_data.put("h",7);
+            piece_data.put("g",6);
+            piece_data.put("f",5);
+            piece_data.put("e",4);
+            piece_data.put("d",3);
+            piece_data.put("c",2);
+            piece_data.put("b",1);
+            piece_data.put("a",0);
 
 
             String[] raw_data1 = pgn_data0.split("]"); //extract piece data from pgn format
@@ -307,7 +253,7 @@ public class PgnParse {
                             row_index = piece_data.get("" + move0.charAt(1)); //charat(2) is '=', example h8=Q
                             piece_value = piece_data.get("" + move0.charAt(3))+ 6 * weight0;
 
-                            setChessTable(piece_value,row_index,col_index); //cur_chess_table[row_index][col_index] = piece_value;
+                            setChessTable(piece_value,getRealRow(row_index),col_index); //cur_chess_table[row_index][col_index] = piece_value;
                             return cur_chess_table;
                         }
 
@@ -326,15 +272,17 @@ public class PgnParse {
                             logger.info("cur char is " + cur_char);
                         }
 
-                        cur_char = move0.charAt(++piece_index); //get row_data from pgn
+                        ++piece_index;
+                        cur_char = move0.charAt(piece_index); //get row_data from pgn
                         row_index = piece_data.get("" + cur_char);
+
                         if (row_index < 0 || row_index >= 8) {
                             logger.info("error in col_index " + row_index);
                             logger.info("cur char is " + cur_char);
                         }
 
-                        cur_chess_table[row_index][col_index] = piece_value;
-
+                        //logger.info(String.valueOf(row_index) + " " + String.valueOf(col_index));
+                        setChessTable(piece_value,getRealRow(row_index),col_index);
 
                         //control piece's previous position
                         boolean previous_move = false;
@@ -348,7 +296,18 @@ public class PgnParse {
 
                             else
                             {
-                                int[][] tmp_array = {{1,0}};
+                                int[][] tmp_array = new int[2][2];
+                                tmp_array[0][0] = 1;
+                                tmp_array[0][1] = 0;
+                                tmp_array[1][0] = 1;
+                                tmp_array[1][1] = 0;
+
+                                if(pawn_first_move[piece_value/7].charAt(col_index) == '0')
+                                {
+                                    tmp_array[1][0] = 2;
+                                    pawn_first_move[piece_value/7].setCharAt(col_index, '1');
+                                }
+
                                 previous_move = calculatePreviousMove(tmp_array,row_index,col_index,piece_value,x_char);
                             }
                         }
@@ -433,8 +392,8 @@ public class PgnParse {
                         }
 
 
-                        if(!previous_move)
-                            logger.info("error in process " + move0);
+                        //if(!previous_move)
+                            //logger.info("error in process " + move0);
 
 
                 } catch (Exception e) {
@@ -477,8 +436,10 @@ public class PgnParse {
 
                 int bargain_row = tmp_array[cnt][0] * black_opt * (-1);
                 int bargain_col = tmp_array[cnt][1] * (-1);
-                int prv_row_index = row_index + bargain_row;
+                int prv_row_index = getRealRow(row_index + bargain_row);
                 int prv_col_index = col_index + bargain_col;
+
+                logger.info(String.valueOf(prv_row_index) + " " + String.valueOf(prv_col_index));
 
                 int match_row_index = prv_row_index;
                 int match_col_index = prv_col_index;
@@ -491,18 +452,32 @@ public class PgnParse {
 
                 if ((top_p <= prv_row_index) && (prv_row_index <= bottom_p)) {
 
+                    //setChessTable(piece_value,prv_row_index,prv_col_index);
+                    //showTableValue(-1);
+
                     if (cur_chess_table[prv_row_index][prv_col_index] == 0)
                     {
-                        continue;
+
                     }
 
                    else if(cur_chess_table[prv_row_index][prv_col_index] == piece_value)
                    {
+                       setChessTable(0,prv_row_index,prv_col_index);
+                       showTableValue(-1);
+
                        if((prv_row_index == match_row_index) && (prv_col_index == match_col_index))
                        {
-                           setChessTable(piece_value,prv_row_index,prv_col_index); //cur_chess_table[prv_row_index][prv_col_index] = 0;
+                           setChessTable(0,prv_row_index,prv_col_index); //cur_chess_table[prv_row_index][prv_col_index] = 0;
+                           //showTableValue(-1);
                            return true;
                        }
+                   }
+
+                   else
+                   {
+                         logger.info("piece_value " + cur_chess_table[prv_row_index][prv_col_index]);
+                        //setChessTable(13,prv_row_index,prv_col_index);
+                        showTableValue(-1);
                    }
                 }
             }
@@ -516,26 +491,36 @@ public class PgnParse {
 
     public static void showTableValue(int where0)
     {
-        for(int row0 = top_p ; row0 <= bottom_p; ++row0)
-        {
-            for(int col0 = left_p ; col0 <= right_p; ++col0)
-            {
-                System.out.print(String.format("%02d ",check_table_mem.get(where0)[row0][col0]));
+        if (where0 >= 0) {
+            for (int row0 = top_p; row0 <= bottom_p; ++row0) {
+                for (int col0 = left_p; col0 <= right_p; ++col0) {
+                    //System.out.print(String.format("%d ",check_table_mem.get(where0)[row0][col0]));
+                    System.out.print(chess_piece_line.charAt(check_table_mem.get(where0)[row0][col0]));
+                }
+                System.out.println();
             }
-
             System.out.println();
+        }
+
+        else {
+            for (int row0 = top_p; row0 <= bottom_p; ++row0) {
+                for (int col0 = left_p; col0 <= right_p; ++col0) {
+                    //System.out.print(String.format("%d ",check_table_mem.get(where0)[row0][col0]));
+                    System.out.print(chess_piece_line.charAt(cur_chess_table[row0][col0]));
+                }
+                System.out.println();
+            }
             System.out.println();
         }
     }
 
-    public static void setChessTable(int piece_value, int row, int col)
+    public static int getRealRow(int row)
     {
-        cur_chess_table[board_size - row][col] = piece_value;
+        return board_size -1 - row;
     }
 
-    public static void main(String[] args){
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        onlyParser(pgn_example2,0,1);
+    public static void setChessTable(int piece_value, int row, int col)
+    {
+        cur_chess_table[row][col] = piece_value;
     }
 }
---
+++ src/main/java/io/github/profrog/main.java
@@ -0,0 +1,92 @@
+package io.github.profrog;
+
+import io.github.profrog.PgnParse;
+
+import java.awt.*;
+import java.util.HashMap;
+
+public class main {
+
+    public static String pgn_example = "\n" +
+            "[Event \"Live Chess\"]\n" +
+            "[Site \"Chess.com\"]\n" +
+            "[Date \"2024.03.23\"]\n" +
+            "[Round \"?\"]\n" +
+            "[White \"MagicRoss\"]\n" +
+            "[Black \"PFchessX2\"]\n" +
+            "[Result \"0-1\"]\n" +
+            "[ECO \"D02\"]\n" +
+            "[WhiteElo \"1139\"]\n" +
+            "[BlackElo \"1150\"]\n" +
+            "[TimeControl \"120+1\"]\n" +
+            "[EndTime \"19:04:44 PDT\"]\n" +
+            "[Termination \"PFchessX2 won by checkmate\"]\n" +
+            "\n" +
+            "1. d4 d5 2. Nf3 Nc6 3. e3 Bf5 4. Bb5 a6 5. O-O e6 6. Bxc6+ bxc6 7. c3 Bd6 8. Re1\n" +
+            "Nf6 9. h3 h6 10. Nh4 Bh7 11. g4 Ne4 12. Nd2 Qxh4 13. Nxe4 Bxe4 14. f3 Qg3+ 15.\n" +
+            "Kf1 Bxf3 16. Qd2 Qxh3+ 17. Kg1 Qh1+ 18. Kf2 Qg2# 0-1\n";
+
+    public static String pgn_example2 = "\n" +
+            "[Event \"Casual 긴 대국 game\"]\n" +
+            "[Site \"https://lichess.org/3FLP6aWA\"]\n" +
+            "[Date \"2024.04.02\"]\n" +
+            "[White \"lichess AI level 1\"]\n" +
+            "[Black \"profrog\"]\n" +
+            "[Result \"0-1\"]\n" +
+            "[UTCDate \"2024.04.02\"]\n" +
+            "[UTCTime \"14:59:54\"]\n" +
+            "[WhiteElo \"?\"]\n" +
+            "[BlackElo \"1500\"]\n" +
+            "[Variant \"Standard\"]\n" +
+            "[TimeControl \"-\"]\n" +
+            "[ECO \"C44\"]\n" +
+            "[Opening \"Ponziani Opening\"]\n" +
+            "[Termination \"Normal\"]\n" +
+            "[Annotator \"lichess.org\"]\n" +
+            "\n" +
+            "1. e4 e5 2. Nf3 Nc6 3. c3 { C44 Ponziani Opening } Qf6 " +
+            "4. d4 exd4 5. cxd4 Bb4+ 6. Ke2 Nh6 7. Nc3 Ng4 8. a3 Nxd4+ 9. Kd2 Bxc3+ 10. Kd3 Nxf2+ " +
+            "11. Kc4 Nxd1 12. Rb1 b5+ 13. Kd5 Bb7+ 14. Kc5 d6# { Black wins by checkmate. } 0-1\n";
+
+
+    public static void main(String[] args){
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        //PgnParse.showTableValue(0);
+        PgnParse.onlyParser(pgn_example,0,1);
+    }
+
+    /*
+    public static void parserInit(Image[] chess_img0, Color boardline_lout0, String pgn_data0, int wanttosee_table)
+    {
+        cur_chess_table = new int[8][8];
+        chess_img = new Image[chess_img0.length];
+        pgn_state = new HashMap<String,String>();
+
+        for(int idx = 0; idx < chess_img0.length; ++idx)
+        {
+            chess_img[idx] = chess_img0[idx];
+        }
+
+        boardline_lout = boardline_lout0;
+        pgn_data = pgn_data0;
+
+        //chess_image[0] = board_image.png..jpg..bmp..
+        //chess_image[1] = white_pawn_image.png..jpg..bmp..
+        //chess_image[2] = white_knight_image.png..jpg..bmp..
+        //chess_image[3] = white_bishop_image.png..jpg..bmp..
+        //chess_image[4] = white_rook_image.png..jpg..bmp..
+        //chess_image[5] = white_queen_image.png..jpg..bmp..
+        //chess_image[6] = white_king_image.png..jpg..bmp..
+
+        //chess_image[7] = black_pawn_image.png..jpg..bmp..
+        //chess_image[8] = black_knight_image.png..jpg..bmp..
+        //chess_image[9] = black_bishop_image.png..jpg..bmp..
+        //chess_image[10] = black_rook_image.png..jpg..bmp..
+        //chess_image[11] = black_queen_image.png..jpg..bmp..
+        //chess_image[12] = black_king_image.png..jpg..bmp..
+    }
+
+    */
+
+}
